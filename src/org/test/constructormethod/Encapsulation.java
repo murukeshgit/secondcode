fc638c999f3439357077370a02e5c255832cb2ee
murukeshgit <murukesh1522@gmail.com>
2025-07-09 12:18:04 +0530

    new code add rajkumar

--- src/org/test/constructormethod/Encapsulation.java
+++ src/org/test/constructormethod/Encapsulation.java
@@ -2,4 +2,18 @@ package org.test.constructormethod;
 
 public class Encapsulation {
 
+	private void sample() {
+		
+		System.out.println("murukesh");
+		
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		Encapsulation a = new Encapsulation();
+		a.sample();
+		
+		
+	}
 }
